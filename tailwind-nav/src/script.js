8bad84ddfa4e396f095c3aa58806ee9591dc2a61
r0bc0ulter <rcoulter05@qub.ac.uk>
2022-11-30 09:32:37 +0000

    nav draft

--- tailwind-nav/src/script.js
+++ tailwind-nav/src/script.js
@@ -6,7 +6,7 @@ document.getElementById("toggle").addEventListener("click", function () {
 const backimage =
   "https://images.unsplash.com/photo-1534237710431-e2fc698436d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
 
-document.getElementById("toggle-search").addEventListener("click", function () {
-  document.getElementById("foot").classList.toggle("-bottom-full");
-  document.getElementById("foot").classList.toggle("bottom-0");
-});
+// document.getElementById("toggle-search").addEventListener("click", function () {
+//   document.getElementById("foot").classList.toggle("-bottom-full");
+//   document.getElementById("foot").classList.toggle("bottom-0");
+// });
