8abecbebae0816ae070b06c29dc39ab3dba19e4e
r0bc0ulter <rcoulter05@qub.ac.uk>
2022-12-06 11:51:20 +0000

    Second toggle button now removed

--- idemnify-react/src/components/Header1.js
+++ idemnify-react/src/components/Header1.js
@@ -55,22 +55,7 @@ function Header1() {
               </a>
             </div>
 
-            <button onClick={toggleburger} id="toggle" className="md:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            </button>
+            <button onClick={toggleburger} id="" className="md:hidden"></button>
 
             <nav
               id="nav"
